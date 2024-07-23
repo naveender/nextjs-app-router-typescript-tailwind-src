a6f232925253cd565da3125910cd7224bbaa31b4
naveender <rajveercoder@gmail.com>
2024-07-23 14:51:10 +0530

    archi setup

---
+++ src/app/error.js
@@ -0,0 +1,7 @@
+"use client"
+
+export default function ErrorPage() {
+  return (
+    <h1> Error Page</h1>
+  );
+}
---
+++ src/app/not-found.js
@@ -0,0 +1,7 @@
+"use client"
+
+export default function notFoundPage() {
+  return (
+    <h1> Hell Not Found...</h1>
+  );
+}
